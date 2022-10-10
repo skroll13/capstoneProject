a25164a75cd599dc7948632734d62567489054e7
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-10 01:32:57 -0500

    Installed Listen Notes API

--- capstone/src/index.js
+++ capstone/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const { Client } = require('podcast-api')
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
