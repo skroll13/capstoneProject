0b57d479f63ebcd052c8722f55c3bb525433f2a3
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-09 21:45:29 -0500

    Made changes to App.css and App.js

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,12 +1,9 @@
-
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-   
-   
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -14,13 +11,12 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
