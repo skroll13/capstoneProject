f0e9333b548b9d0f5ddb4fdcc46e700c7c65cf32
Shannon Kroll <skroll912@gmail.com>
2022-10-11 12:32:43 -0700

    backend initial set up complete

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+//middlewares
+
+
+//routes
+
+//error handlers
+
+//listen to server
+const PORT = process.env.PORT || 9000
+app.listen(PORT, console.log(`listening on port ${PORT}`))
