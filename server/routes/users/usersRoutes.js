052854a6e944c394ac7cca704f424f4141d9ae23
Shannon Kroll <skroll912@gmail.com>
2022-10-13 11:25:42 -0700

    backend token handling logic complete

--- server/routes/users/usersRoutes.js
+++ server/routes/users/usersRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
+
 const { registerUserController, 
     userLoginController, 
     userProfileController, 
     deleteUserController, 
     updateUserController 
 } = require('../../controllers/users/usersController')
+const isLogin = require('../../middleware/isLogin')
 
 const usersRoute = express.Router()
 
@@ -14,8 +16,8 @@ usersRoute.post("/register", registerUserController)
 //POST/api/v1/users/login
 usersRoute.post("/login", userLoginController)
 
-//GET/api/v1/users/profile/:id
-usersRoute.get("/profile/:id", userProfileController)
+//GET/api/v1/users/profile
+usersRoute.get("/profile/", isLogin, userProfileController)
 
 //DELETE/api/v1/users/:id
 usersRoute.delete("/:id", deleteUserController)
