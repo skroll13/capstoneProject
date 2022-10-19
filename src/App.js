19f33704d04bcd3ff117a535bd2ed27b3a7f1a9e
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-18 23:39:53 -0500

    styling mockups

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css"
 import React from 'react'
-import pic from './assets/mohammad-metri-1oKxSKSOowE-unsplash.jpg'
+// import pic from './assets/mohammad-metri-1oKxSKSOowE-unsplash.jpg'
 
 function App() {
   return (
       <>
     
 
-     {/* <h1 className='font-mono text-red-700'>Welcome to Your Podcasts</h1> */}
-
-      <img className="img-fluid" src={pic} value="mohammad-metri-1oKxSKSOowE-unsplash" alt="neon sign"/>
+   
+    
+      {/* <img className="img-fluid" src={pic} value="mohammad-metri-1oKxSKSOowE-unsplash" alt="neon sign"/> */}
       <div className="background-color: bg-red-600" variant="light">
         Photo by <a href="https://unsplash.com/@mohammadmetri?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="link-dark">Mohammad Metri</a> on <a href="https://unsplash.com/s/photos/podcast?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="link-dark">Unsplash</a>
- main
+ 
       </div>
 
      
