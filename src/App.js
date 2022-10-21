77a12f07e6c707f28ef3292bcaa052f2fbbb1ba6
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-20 22:42:03 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
 import './App.css'
 import React from 'react'
-// import pic from './assets/mohammad-metri-1oKxSKSOowE-unsplash.jpg'
+import pic from './assets/mohammad-metri-1oKxSKSOowE-unsplash.jpg'
 
 function App () {
   return (
-    <>
-
-     <div className="bg-black h-96 w-full bg-cover bg-center p-24">
-    
-    <img src={pic}></img>
-    <div className='p-24'></div>
-
-    <h1 className="text-white' text-6xl font-bold">Welcome to YourPodcasts</h1>
-
-    <h2 className="text-black text-3xl font-light">Still so much to do</h2>
-    
-
-    </div>
-
+    <div>
+      <>
+        <img src='./assets/mohammad-metri-1oKxSKSOowE-unsplash.jpg' className='w-full h-full object-cover overlay' />
+        <div className='p-24'>
+          <h1 className='text-green-400 text-6xl font-bold'>Welcome to YourPodcasts!</h1>
+        </div>
+     
+    </>
     </div>
   )
-}
-
-{
-  /* /* <div className='has-bg-img background-color: bg-red-600' variant='light'>
-      <Menu  />
-        <img
-          className="bg-img"
-          
-          src="mohammad-metri-1oKxSKSOowE-unsplash.jpg"
-          
-          alt='neon sign'
-        />
-      </div>
-      </>
-      )} */
-}
+}  
+   
 
 export default App
