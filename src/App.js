22fab7e4cc85f675dec1b37f1bed383c4e169c91
Yvonne Zertuche <94756952+YvonneOZertuche@users.noreply.github.com>
2022-10-21 08:46:04 -0500

    Landing page is working again

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css'
 import React from 'react'
-import {pic1} from './components/images/pic1.jpg'
+
+import pic1 from './components/images/pic1.jpg'
+
 
 function App () {
   return (
     <>
-      <div className='background-color: bg-red-400' variant='light'></div>
-
-        <div className = "object-cover z-40 bg-fixed'" style={{backgroundImage: `url(${"/components/images/pexels-anna-tarazevich-wide.jpg"})`, backgroundRepeat:"no-repeat"}}></div>
-      
-
+      <div className='background-color: bg-red-600' variant='light'>
+        <img
+          className='image-fluid justify-content-center'
+          src={pic1}
+          value='mohammad-metri-1oKxSKSOowE-unsplash'
+          alt='neon sign'
+        />
+      </div>
     </>
   )
 }
